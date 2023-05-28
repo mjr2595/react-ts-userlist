14611f106e86615f97effcd8525a06a39242b9d2
mjr2595 <michaeljohnraymond@gmail.com>
2023-05-28 13:03:43 -0500

    customer hook useUsers

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,8 @@
-import { useEffect, useState } from "react";
-import { CanceledError } from "./services/api-client";
 import userService, { User } from "./services/user-service";
+import useUsers from "./hooks/useUsers";
 
 function App() {
-  const [users, setUsers] = useState<User[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    // const fetchUsers = async () => {
-    //   try {
-    //     const res = await axios.get<User[]>("https://jsonplaceholder.typicode.com/users");
-    //     setUsers(res.data);
-    //   } catch (err) {
-    //     setError((err as AxiosError).message);
-    //   }
-    // };
-    // fetchUsers();
-
-    // get -> await promise -> res / err
-
-    setLoading(true);
-    const { request, cancel } = userService.getAll<User>();
-    request
-      .then((res) => {
-        setUsers(res.data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
-
-    return () => cancel();
-  }, []);
+  const { users, error, isLoading, setUsers, setError } = useUsers();
 
   const addUser = () => {
     const originalUsers = [...users];
